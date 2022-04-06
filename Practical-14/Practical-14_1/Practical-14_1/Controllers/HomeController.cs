f13037c9536398be2ed34d0525849e2f37b0a149
SarveshSimform <sarvesh.p@simformsolutions.com>
2022-04-06 16:50:52 +0530

    Practical 14 Uploaded.

---
+++ Practical-14/Practical-14_1/Practical-14_1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical_14_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //by default index page is access because current user is login
+       /// <summary>
+       /// Windows authentication is allow to access index page to current User
+       /// </summary>
+       /// <returns></returns>
+        [Authorize(Users = @"SF-CPU-493\sarvesh")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// if user access this page than first user must be login to Simfromsolution credential then after access this page
+        /// </summary>
+        /// <returns></returns>
+        [Authorize(Users = @"SF-CPU-493\Simfromsolutions")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// if user access this page than first user must be login to Simfromsolution credential then after access this page
+        /// </summary>
+        /// <returns></returns>
+
+        [Authorize(Users = @"SF-CPU-493\Simfromsolutions")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ Practical-14/Practical-14_2/Practical-14_2/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using Practical_14_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Practical_14_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //database context file
+        /// <summary>
+        /// initialized object
+        /// </summary>
+        dbauthenticationEntities db = new dbauthenticationEntities();
+
+        /// <summary>
+        /// authorize attribute filter checks the user is login or not 
+        /// if user is not login than redirect login page
+        /// </summary>
+        /// <returns></returns>
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+   
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Tbl_Admim tbl_Admim)
+        {
+            //check user is exist or not
+            var User = db.Tbl_Admim.FirstOrDefault(x => x.UserName == tbl_Admim.UserName && x.PassWord == tbl_Admim.PassWord);
+            //   
+            if (User == null)
+            {
+                    //display message to client side if user not exist
+                   ViewBag.Message="The user name or password provided is incorrect.";
+            }
+            else
+            {
+                //here FormsAuthentication is saved cookie to browser by username         
+                FormsAuthentication.SetAuthCookie(tbl_Admim.UserName,true);
+                return RedirectToAction("index");
+             }
+            return View(tbl_Admim);
+        }
+
+        [Authorize]
+        public ActionResult SignOut()
+        {
+            //here FormsAuthentication.SignOut() is SignOut the current
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+    }
+}
