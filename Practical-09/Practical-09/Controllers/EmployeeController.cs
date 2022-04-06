05466c9edabbb487e3d93b2e4a0b973beb5c13f2
SarveshSimform <sarvesh.p@simformsolutions.com>
2022-04-06 14:53:56 +0530

    Practical 9 Added

---
+++ Practical-09/Practical-09/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using Practical_09.Filters;
+using Practical_09.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exception = Practical_09.Filters.Exception;
+
+namespace Practical_09.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+
+        // GET: Employee
+        [Route("Employee/", Name = "Mark")]
+        public string Index(string Name)
+        {
+            return "Hello " + Name;
+        }
+
+        public string HelloWorld ()
+        {
+            return "Hello Mark";
+        }
+
+
+        public ActionResult OutPutCache()
+        {
+            return View();
+        }
+
+        public ActionResult Divide()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Practical-09/Practical-09/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using Practical_09.Filters;
+using Practical_09.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exception = Practical_09.Filters.Exception;
+
+namespace Practical_09.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Employee> emp;
+        /// <summary>
+        /// HomeController has Constructor to assign some predefined values of lists
+        /// </summary>
+        public HomeController()
+        {
+            emp = new List<Employee>()
+            {
+                    new Employee{Id=1,Ename="Sarvesh" , Eage=21 },
+                    new Employee{Id=2,Ename="Keyur" , Eage=20 },
+                    new Employee{Id=3,Ename="Dev" , Eage=29 },
+            };
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ContentResult contentResult()
+        {
+            return Content("<b>Hello</b>, World! this message is from Employee Controller using the Action Result, <script> alert('Hi! I am Sarvesh Patel') </script>");
+        }
+
+        public JsonResult JsonResult()
+        {
+            var output = emp;
+            return Json(output, JsonRequestBehavior.AllowGet);
+        }
+        public EmptyResult emptyResult()
+        {
+            return new EmptyResult();
+        }
+        [HttpGet]
+        public JavaScriptResult javaScriptResult()
+        {
+            return JavaScript("alert('Hello')");
+        }
+
+        public FileResult FileResult()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(Server.MapPath("~/ReadFile.txt"));
+            return File(fileBytes, "text/plain");
+        }
+
+        public RedirectResult RedirectResult()
+        {
+            return Redirect("https://localhost:44348/Employee/HelloWorld/");
+        }
+
+        /// <summary>
+        /// Use OutPutCache filter
+        /// date-time will be displayed for an interval of 5 minutes
+        /// </summary>
+        /// <returns></returns>
+        [OutputCache(Duration = 300)]
+        public ActionResult OutPutCache()
+        {
+            TempData["Date&Time"] = System.DateTime.Now.ToString();
+            return RedirectToAction("OutPutCache","Employee" );
+
+        }
+
+        //call input page
+        [HttpGet]
+        public ActionResult Dividebyzero()
+        {
+            return RedirectToAction("Divide", "Employee");
+        }
+
+        //use custom exception filter
+        [Exception]
+        [HttpPost]
+        //user input data and submit form than this method invoked
+        public ActionResult Divide(DivideByZero divideByZero)
+        {
+            divideByZero.Reslut = divideByZero.Num1 / divideByZero.Num2;
+            string Reslut = divideByZero.Reslut.ToString();
+            ViewBag.Reslut = "Your Output is " + Reslut;
+            return RedirectToAction("Divide", "Employee");
+        }
+    }
+}
